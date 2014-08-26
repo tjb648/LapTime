745570a5072ff644385fe0ca276b81b30a267ded
tjb648 <tjb648@uowmail.edu.au>
2014-08-26 17:14:05 +1000

    First commit assignment1

---
+++ src/com/example/LapTime/TimerActivity.java
@@ -0,0 +1,15 @@
+package com.example.LapTime;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TimerActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
---
+++ src/com/example/LapTime/TimesFragment.java
@@ -0,0 +1,17 @@
+package com.example.LapTime;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Tyson on 8/26/2014.
+ */
+public class TimesFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.timeslog, container, false);
+    }
+}
