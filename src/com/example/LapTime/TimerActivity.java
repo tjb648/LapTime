3000a82cca31a12acdd165254c8d2c5dfd528a0e
tjb648 <tjb648@uowmail.edu.au>
2014-08-26 22:26:56 +1000

    Second commit

--- src/com/example/LapTime/TimerActivity.java
+++ src/com/example/LapTime/TimerActivity.java
@@ -1,15 +1,33 @@
 package com.example.LapTime;
 
 import android.app.Activity;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class TimerActivity extends Activity
-{
-    /** Called when the activity is first created. */
+
+public class TimerActivity extends Activity {
+
+    /**
+     * Called when the activity is first created.
+     */
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        TimerFragment fragment = new TimerFragment();
+        fragmentTransaction.add(R.id.frame, fragment);
+
     }
+
 }
+
+
+
--- src/com/example/LapTime/TimerFragment.java
+++ src/com/example/LapTime/TimerFragment.java
@@ -1,17 +1,57 @@
 package com.example.LapTime;
 
+
 import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * Created by Tyson on 8/26/2014.
  */
 public class TimerFragment extends Fragment {
+
+    private Button btnSS;
+    private double time;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.timerfrag, container, false);
+
+        View rootView = inflater.inflate(R.layout.timerfrag, container, false);
+
+        initFrag();
+
+
+       return rootView;
+
+      // return inflater.inflate(R.layout.timerfrag, container, false);
+
     }
+
+
+    public void initFrag()
+    {
+        time = 00.00;
+    //    (Button) getView().findViewById(R.id.timer) = time;
+        btnSS = (Button) getView().findViewById(R.id.BTstart);
+        btnSS.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                timerButton();
+            }
+        });
+
+    }
+
+    public void timerButton(){
+
+        if(btnSS.getText()== "Start")
+            btnSS.setText("Stop");
+            else if(btnSS.getText()=="Stop")
+                btnSS.setText("Start");
+            else if(btnSS.getText()=="Stop")
+                btnSS.setText("Reset");
+    }
+
 }
