d171bf3562a64958d7914241a12a656f950f0290
tjb648 <tjb648@uowmail.edu.au>
2014-08-31 13:57:08 +1000

    git xfer after comp death

--- src/com/example/LapTime/TimerFragment.java
+++ src/com/example/LapTime/TimerFragment.java
@@ -8,26 +8,37 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.os.Handler;
-
+import org.w3c.dom.Text;
 
 
 /**
  * Created by Tyson on 8/26/2014.
  */
 public class TimerFragment extends Fragment {
-
+    //Button
    private Button btnSS;
+    //TextViews
+   private TextView tBest;
+   private TextView tWorst;
+   private TextView tAve;
    private TextView timerValue;
+ //Bar layouts
+  LinearLayout layBest = null;
+  LinearLayout layWorst = null;
+  LinearLayout layAverage = null;
+  LinearLayout layCurr = null;
+  //Timer longs and ints
    private long stTime = 0L;
    long miliSecs = 0L;
    long timeBuff = 0L;
    long timeUpdate = 0L;
-
    int btCheck = 0;
 
-
+    //to work out best average worst maybe concatenate string timervalue into 1 big int and compare
   private Handler someHandler = new Handler();
 
 
@@ -36,10 +47,15 @@ public class TimerFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View rootView = inflater.inflate(R.layout.timerfrag, container, false);
-
-        timerValue = (TextView) rootView.findViewById(R.id.timer);
         btnSS = (Button) rootView.findViewById(R.id.BTstart);
+        //Displayed time
+        timerValue = (TextView) rootView.findViewById(R.id.timer);
 
+        tBest = (TextView) rootView.findViewById(R.id.best);
+        tWorst = (TextView) rootView.findViewById(R.id.worst);
+        tAve = (TextView) rootView.findViewById(R.id.average);
+
+        layBest = (LinearLayout) rootView.findViewById(R.id.barBest);
 
         btnSS.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -48,50 +64,36 @@ public class TimerFragment extends Fragment {
                if(btCheck == 0) {
                    stTime = SystemClock.uptimeMillis();
                    someHandler.postDelayed(upDateTimer, 0);
+
                    btCheck++;
-                   timerButton();
+                   btnSS.setText("Stop");
+
                }else if (btCheck == 1){
                    timeBuff += miliSecs;
                    someHandler.removeCallbacks(upDateTimer);
                    btCheck++;
-                   timerButton();
+                   tBest.setText("Best:" + timerValue.getText());
+
+                   btnSS.setText("Reset");
+
                }else if (btCheck == 2){
-
-                    System.out.println(timerValue.getText());
-
-
 
                    stTime = 0L;
                    miliSecs = 0L;
                    timeBuff = 0L;
                    timeUpdate = 0L;
                    btCheck = 0;
-                   timerButton();
+
+                   btnSS.setText("Start");
+
+
 
                }
             }
         });
 
+         return rootView;
 
-
-
-
-
-        return rootView;
-
-    }
-
-
-
-    public void timerButton() {
-
-        if (btnSS.getText() == "Start") {
-            btnSS.setText("Stop");
-        } else if (btnSS.getText() == "Stop") {
-            btnSS.setText("Reset");
-        } else if (btnSS.getText() == "Reset") {
-            btnSS.setText("Start");
-        }
     }
 
     private Runnable upDateTimer = new Runnable() {
@@ -114,4 +116,22 @@ public class TimerFragment extends Fragment {
         }
     };
 
+    public void timeCalc(){
+
+        // should i change variables to global or pull apart string
+        String minutes, seconds, tiny_seconds;
+
+        String buffer;
+
+        buffer = timerValue.getText().toString();
+
+        minutes = buffer.substring(0,1);
+        seconds = buffer.substring(3, 4);
+        tiny_seconds = buffer.substring(6,7);
+
+
+
+
+
+    }
 }
